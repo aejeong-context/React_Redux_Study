2ad6fae2587b9153e90740d1085af90044e0dc68
aejeong <aejeong-context@kakao.com>
2020-08-14 12:34:15 +0900

    create-project

---
+++ first-react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Second from 'second';
+function App() {
+
+  const text='애정이';
+
+  return (
+  <div>
+    <Second />
+    {text} 안녕?</div>
+  
+  
+  );
+}
+
+export default App;
---
+++ first-react/src/second.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function second() {
+
+  const text='지수';
+
+  return (
+  <div>
+    {text} 안녕?</div>
+  );
+}
+
+export default second;
